a63b1dc0b737574bc844d5b4408b7bc95519adfd
lxw <2332425872@qq.com>
2022-07-22 11:22:18 +0800

    first commit

---
+++ xxljob-demo/src/test/java/com/xiaowei/xxljobdemo/XxljobDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaowei.xxljobdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxljobDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
